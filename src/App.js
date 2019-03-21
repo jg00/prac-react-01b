f909426fe298a51da25d6a42914a57e013bc2fa2
Jack Gamboa <jgamboa929@gmail.com>
2019-03-21 10:39:37 -0500

    Props and State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  // Class' Component State.  We use this to manage state from inside a component.  Props was used to get information from the outside.
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 }
+    ]
+  };
+
+  /*
+    Note: If we change this special property called state, it will lead React to re-render
+    (update) our DOM.
+
+  */
+
+  /*
+    Note: With React 16.8 a new feature called 'React Hooks' that will allow us to also manage state
+    in 'functional components' like we do here in 'class components' (default way of adding it).
+  */
+
   render() {
     return (
       <div className="App">
@@ -13,11 +33,22 @@ class App extends Component {
           just syntactic sugar for JavaScript, allowing you to write HTMLish
           code instead of nested React.createElement(...) calls.
         </p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">
+        <button>Switch Name</button>
+        {/* <Person name="Max" age="28" /> */}
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name="Stephanie" age="26" />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
 
