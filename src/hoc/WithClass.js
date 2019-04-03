96812346ac7b47bd30711fe718de9d2e3f1d0f8d
Jack Gamboa <jgamboa929@gmail.com>
2019-04-03 15:19:17 -0500

    HOCs

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -13,7 +13,15 @@
         
         In the regular Javascript function, the first argument will actually be our 'WrappedComponent' (ie a reference to
         a component), and the second argument (or more, passed as props), is something that 
-        you need in your HOC.     
+        you need in your HOC. 
+
+    Which way to use? Depends.
+      1 First way where you wrap your JSX component in App.js may be good for 
+        styling and/or some HTML changes.
+    
+      2 Second way where you wrap your entired component may be better if you want to add some
+        'behind the scenes logic'.  Some Javascript code that handles errors or sends some anylytics
+        data, etc.
 */
 
 import React from "react";
